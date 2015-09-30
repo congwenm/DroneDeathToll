497c69fcee67263b1516ddc53013e3bcc0ab9b97
eric ma <lostaznfound@gmail.com>
2015-09-30 01:24:41 -0500

    clean up

--- index.jsx
+++ index.jsx
@@ -10,13 +10,6 @@ var d3 = require('./d3/d3.js');
 var App = require('./src/app.jsx');
 var css = require('./src/style.css')
 
-var sampleData = [
-  {id: '5fbmzmtc', x: 7, y: 41, z: 6},
-  {id: 's4f8phwm', x: 11, y: 45, z: 9},
-  {id: 's1f8phwm', x: 21, y: 40, z: 9},
-  {id: 's2f8phwm', x: 15, y: 45, z: 9},
-  {id: 's3f8phwm', x: 5, y: 35, z: 20},
-];
 
 var Index = React.createClass({
   render: function () {
